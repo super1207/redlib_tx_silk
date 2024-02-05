a2936d906e0d2ace9509d97116ca6de1e691287f
super1207 <1875159423@qq.com>
2024-02-05 13:05:07 +0800

    init

---
+++ build.bat
@@ -0,0 +1,10 @@
+set RUSTFLAGS='-C target-feature=+crt-static'
+cargo install cross
+rustup target add i686-pc-windows-msvc
+rustup target add x86_64-pc-windows-msvc
+cargo build --target=i686-pc-windows-msvc --release
+cargo build --target=x86_64-pc-windows-msvc --release
+cross build --target x86_64-unknown-linux-gnu --release
+cross build --target i686-unknown-linux-gnu --release
+cross build --target aarch64-linux-android --release
+cross build --target aarch64-unknown-linux-gnu --release
